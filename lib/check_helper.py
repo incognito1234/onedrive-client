b320965d4d59c332c37b283657d4fae8ebd54153
Jareth Lomson <jareth.lomson@gmail.com>
2020-10-22 21:40:08 +0200

    Better compatibility with python 3.5

--- lib/check_helper.py
+++ lib/check_helper.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+from shutil import which
 
 
 class quickxorhash:
@@ -7,38 +8,18 @@ class quickxorhash:
   __COMMAND_NAME = 'quickxorhash'
 
   def __init__(self):
-    list_folder = where(quickxorhash.__COMMAND_NAME)
-    if len(list_folder) != 1:
-      print('pbm')
-      self.program = None
-    else:
-      self.program = "{0}".format(list_folder[0])
+    self.program = which(self.__COMMAND_NAME)
 
   def quickxorhash(self, filename):
 
     if self.program is not None:
-      p = subprocess.run([self.program, filename], capture_output=True)
+      p = subprocess.run([self.program, filename], stdout=subprocess.PIPE)
       if p.returncode != 0:
         return None
       else:
         return str(p.stdout, 'utf-8')[:-1]
     else:
       return None
-
-
-def where(name, flags=os.F_OK):
-    #
-    # 18/10/2020
-    # From: https://codereview.stackexchange.com/questions/123597/find-a-specific-file-or-find-all-executable-files-within-the-system-path
-    #
-  result = []
-  paths = os.defpath.split(os.pathsep)
-  for outerpath in paths:
-    for innerpath, _, _ in os.walk(outerpath):
-      path = os.path.join(innerpath, name)
-      if os.access(path, flags):
-        result.append(os.path.normpath(path))
-  return result
 
   # How to get quickxorhash command
   #
