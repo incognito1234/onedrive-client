910decc911a02d2f4b9b2d313ba08c3989863252
Jareth Lomson <jareth.lomson@gmail.com>
2022-08-11 10:55:34 +0200

    [Browser]Parametrize name column size

--- lib/shell_helper.py
+++ lib/shell_helper.py
@@ -166,39 +166,6 @@ class MsFolderInfo(MsObject):
   def folders_have_been_retrieved(self):
     return self.__children_folders_retrieval_status == "all"
 
-  def print_children(
-          self,
-          start_number=0,
-          recursive=False,
-          only_folders=True,
-          depth=999):
-    if ((not self.folders_have_been_retrieved() and only_folders)
-            or (not self.files_have_been_retrieved() and not only_folders)):
-      self.__mgc.logger.log_debug(
-          f"[print_children] folder_path = {self.get_full_path()} - folders have not been retrieved")
-      self.retrieve_children_info(
-          only_folders=only_folders,
-          recursive=recursive,
-          depth=depth)
-
-    i = start_number
-    for c in self.children_folder:
-      print(f"{i:>3} - {c}")
-      i = i + 1
-    if not only_folders:
-      for c in self.children_file:
-        print(f"{i:>3} - {c}")
-        i = i + 1
-
-    if recursive and depth > 0:
-      for c in self.children_folder:
-        nb_children = c.print_children(
-            start_number=i, recursive=False, depth=depth - 1)
-
-        i += nb_children
-
-    return i - start_number
-
   def __str__(self):
     status_subfolders = "<subfolders ok>" if self.folders_have_been_retrieved() else ""
     status_subfiles = "<subfiles ok>" if self.files_have_been_retrieved() else ""
@@ -253,9 +220,9 @@ class MsFileInfo(MsObject):
   name = property(_get_name)
 
   def __str__(self):
-    fname = f"{self.name}" if len(self.name) < 25 else f"{self.name[:20]}..."
+    fname = f"{self.name}" if len(self.name) < 45 else f"{self.name[:40]}..."
     fmdt = self.last_modified_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    result = f"{self.size:>20,}  {fname:<25}  {fmdt}  "
+    result = f"{self.size:>20,}  {fname:<45}  {fmdt}  "
     return result
 
   def str_full_details(self):
@@ -288,6 +255,12 @@ class OneDriveShell:
   def __init__(self, mgc):
     self.mgc = mgc
     self.only_folders = False
+    self.file_formatter = MsFileFormatter(45)
+    self.folder_formatter = MsFolderFormatter(45)
+
+  def change_max_column_size(self, nb):
+    self.file_formatter = MsFileFormatter(nb)
+    self.folder_formatter = MsFolderFormatter(nb)
 
   def launch(self):
      # current_folder_info = mgc.get_folder_info("")
@@ -301,8 +274,33 @@ class OneDriveShell:
       if my_input == "quit":
         break
 
-      if my_input.isdigit() and int(my_input) <= len(
-              current_folder_info.children_folder):
+      if my_input == "ls":
+        if current_folder_info.parent is not None:
+          print("  0 - <parent>")
+        self.print_folder_children(
+            current_folder_info,
+            start_number=1,
+            only_folders=self.only_folders)
+
+      elif my_input == "set onlyfolders" or my_input == "set of":
+        self.only_folders = True
+
+      elif my_input == "set noonlyfolders" or my_input == "set noof":
+        self.only_folders = False
+
+      elif my_input[:7] == "set cs=" or my_input[:15] == "set columnsize=":
+        str_cs = my_input[7:] if my_input[:7] == "set cs=" else my_input[15:]
+
+        if not str_cs.isdigit():
+          print("<value> of column size must be a digit")
+        else:
+          int_cs = int(str_cs)
+          if (int_cs < 5) or (int_cs > 300):
+            print("<value> must be a number between 5 and 300")
+          else:
+            self.change_max_column_size(int_cs)
+
+      elif my_input.isdigit() and int(my_input) <= len(current_folder_info.children_folder):
 
         int_input = int(my_input)
         if int_input == 0:
@@ -310,18 +308,6 @@ class OneDriveShell:
         else:
           current_folder_info = current_folder_info.children_folder[int(
               my_input) - 1]
-
-      elif my_input == "ls":
-        if current_folder_info.parent is not None:
-          print("  0 - <parent>")
-        current_folder_info.print_children(
-            start_number=1, only_folders=self.only_folders)
-
-      elif my_input == "set onlyfolders" or my_input == "set of":
-        self.only_folders = True
-
-      elif my_input == "set noonlyfolders" or my_input == "set noof":
-        self.only_folders = False
 
       elif my_input == "help" or my_input == "h":
         print("Onedrive Browser Help")
@@ -331,8 +317,86 @@ class OneDriveShell:
         print("   set of                : Retrieve info only about folders")
         print("   set noonlyfolders")
         print("   set noof              : Retrieve info about folders and files")
+        print("   set columnsize")
+        print("   set cs                : Set column size for name")
         print("   ls                    : List current folder")
         print("   <number>              : Dig into given folder")
         print("   quit                  : Quit Browser")
 
       print("")
+
+  def print_folder_children(
+          self,
+          fi,
+          start_number=0,
+          recursive=False,
+          only_folders=True,
+          depth=999):
+    if ((not fi.folders_have_been_retrieved() and only_folders)
+            or (not fi.files_have_been_retrieved() and not only_folders)):
+      self.mgc.logger.log_debug(
+          f"[print_children] folder_path = {fi.get_full_path()} - folders have not been retrieved")
+      fi.retrieve_children_info(
+          only_folders=only_folders,
+          recursive=recursive,
+          depth=depth)
+
+    i = start_number
+    for c in fi.children_folder:
+      print(f"{i:>3} - {self.folder_formatter.format(c)}")
+      i = i + 1
+    if not only_folders:
+      for c in fi.children_file:
+        print(f"{i:>3} - {self.file_formatter.format(c)}")
+        i = i + 1
+
+    if recursive and depth > 0:
+      for c in fi.children_folder:
+        nb_children = c.print_children(
+            start_number=i, recursive=False, depth=depth - 1)
+
+        i += nb_children
+
+    return i - start_number
+
+
+class InfoFormatter(ABC):
+
+  @abstractmethod
+  def format(self, what):
+    return "default"
+
+  @staticmethod
+  def alignright(what, nb, fillchar=" "):
+    return f"{(fillchar * (nb - len(what)))}{what}"
+
+
+class MsFolderFormatter(InfoFormatter):
+
+  def __init__(self, max_name_size=25):
+    self.max_name_size = max_name_size
+
+  @beartype
+  def format(self, what: MsFolderInfo):
+    status_subfolders = "<subfolders ok>" if what.folders_have_been_retrieved() else ""
+    status_subfiles = "<subfiles ok>" if what.files_have_been_retrieved() else ""
+
+    fname = f"{what.name}/" if len(
+        what.name) < self.max_name_size else f"{what.name[:self.max_name_size - 5]}.../"
+    result = (
+        f"{what.size:>20,}  {InfoFormatter.alignright(fname,self.max_name_size)}"
+        f"  {what.child_count:>6}  {status_subfolders}{status_subfiles}")
+    return result
+
+
+class MsFileFormatter(InfoFormatter):
+  def __init__(self, max_name_size=25):
+    self.max_name_size = max_name_size
+
+  @beartype
+  def format(self, what: MsFileInfo):
+    fname = f"{what.name}" if len(
+        what.name) < self.max_name_size else f"{what.name[:self.max_name_size - 5]}..."
+    fmdt = what.last_modified_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    result = f"{what.size:>20,}  {InfoFormatter.alignright(fname,self.max_name_size)}  {fmdt}  "
+    return result
